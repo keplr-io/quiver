ccc89475559ec80fe59ac342a5ec549bd265dfb2
jake <jake@keplr.io>
2016-11-12 21:20:26 -0800

    dev: add client boilerplate, working backend

--- quiver_engine/server.py
+++ quiver_engine/server.py
@@ -10,8 +10,12 @@ from keras.models import Model
 from keras import backend as K
 
 from imagenet_utils import preprocess_input
-from os.path import abspath
+from gevent.wsgi import WSGIServer
+
 from scipy.misc import imsave
+from os.path import abspath, relpath
+import tensorflow as tf
+graph = tf.get_default_graph()
 
 # util function to convert a tensor into a valid image
 def deprocess_image(x):
@@ -27,9 +31,10 @@ def deprocess_image(x):
     return x
 
 
-def get_app(model, session, temp_folder='/tmp'):
+def get_app(model, temp_folder='./tmp'):
 
     app = Flask(__name__)
+    app.threaded=True
     CORS(app)
 
     @app.route('/')
@@ -41,36 +46,51 @@ def get_app(model, session, temp_folder='/tmp'):
         print('lol')
         return send_from_directory(abspath(temp_folder), path)
 
-    @app.route('/layer/<layer_name>', methods=['GET'])
-    def get_model(layer_name):
+    @app.route('/layer/<layer_name>/<input_path>', methods=['GET'])
+    def get_model(layer_name, input_path):
 
         layer_model = Model(
             input=model.input,
             output=model.get_layer(layer_name).output
         )
 
-        img_path = 'elephant.jpg'
+        img_path = input_path
         img = image.load_img(img_path, target_size=(224, 224))
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
 
-        with session as sess:
+        with graph.as_default():
+
             layer_outputs = layer_model.predict(x)
             output_files = []
 
             for z in range(0, layer_outputs.shape[2]):
 
-                img = layer_model.predict(x)[0][:, :, z]
+                img = layer_outputs[0][:, :, z]
                 deprocessed = deprocess_image(img)
                 filename = get_output_name(temp_folder, layer_name, z)
-                output_files.append(filename)
+                output_files.append(
+                    relpath(
+                        filename,
+                        abspath(temp_folder)
+                    )
+                )
                 imsave(filename, deprocessed)
 
-            return jsonify(output_files)
+        return jsonify(output_files)
 
 
     return app
+
+def run_app(app):
+    http_server = WSGIServer(('', 5000), app)
+    return http_server.serve_forever()
+
+def launch(model, temp_folder='./tmp', port=5000):
+    return run_app(
+        get_app(model, temp_folder)
+    )
 
 def get_output_name(temp_folder, layer_name, z_idx):
     return temp_folder + '/' + layer_name + '_' + str(z_idx) + '.png'
