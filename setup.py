5d289857464f1363639005b57ece061a1df200fb
jake <jake@keplr.io>
2016-11-14 19:03:50 -0800

    dist: use manifest.in to include build files

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='quiver_engine',
-    version="0.1.2",
+    version="0.1.3.1",
     author="Jake Bian",
     author_email="jake@keplr.io",
     description=("Interactive per-layer visualization for convents in keras"),
     license='mit',
     packages=find_packages(),
-    package_data={'': ['quiverboard/dist/**/*']},
-    include_package_data=True,
     install_requires=[
         'keras',
         'flask',
